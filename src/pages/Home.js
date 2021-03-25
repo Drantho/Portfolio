3b88bd4b98ad72a9c2a4f778bc1fe1fa17db27ce
Drantho <drantho@gmail.com>
2021-03-25 16:13:35 -0700

    intro style updates

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,5 @@
 import { React, useState } from 'react'
-import { Carousel, Row, Col } from "antd"
+import { Carousel, Row, Col, Image } from "antd"
 
 export default function Home() {
 
@@ -16,18 +16,18 @@ export default function Home() {
             <section className="section" id="intro">
 
 
-                <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }} style={{border: "2px solid green"}}>
+                <Row gutter={{ xs: 16, sm: 32, md: 48, lg: 64 }}>
                     <Col className="gutter-row" xl={8} lg={8} md={8} sm={24}>
                         <img className="portrait" src="https://res.cloudinary.com/drantho/image/upload/v1614876275/jtlw9xzwwwwgayavxqyk.jpg" />
                     </Col>
                     <Col className="gutter-row" xl={16} lg={16} md={16} sm={24}>
-                        <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
-                            <Col className="gutter-row" span={24}>
+                        <Row gutter={{ xs: 16, sm: 32, md: 48, lg: 64 }} justify="center">
+                            <Col className="gutter-row" xl={20} lg={20} md={20} sm={24}>
                                 <h2 className="intro-text">My name is Anthony and I build websites that {verbs[currentVerb]}</h2>
                             </Col>
                         </Row>
-                        <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
-                            <Col className="gutter-row" span={24}>
+                        <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }} justify="center">
+                            <Col className="gutter-row" xl={20} lg={20} md={20} sm={24}>
                                 <Carousel
                                     autoplay
                                     autoplaySpeed={4500}
@@ -39,13 +39,13 @@ export default function Home() {
                                         <video muted autoplay="true" loop="true" src="https://res.cloudinary.com/drantho/video/upload/v1615510871/ezgif.com-gif-maker_cjf7al.webm" />
                                     </div>
                                     <div className="panel">
-                                        <img src="https://res.cloudinary.com/drantho/image/upload/v1614075793/nobrainermeals_e1bpca.png" />
+                                        <img src="https://res.cloudinary.com/drantho/image/upload/c_fill,w_670/v1614075793/nobrainermeals_e1bpca.png" />
                                     </div>
                                     <div className="panel">
-                                        <img src="https://res.cloudinary.com/drantho/image/upload/v1616638401/mervin_graph_ijbjjr.png" />
+                                        <img src="https://res.cloudinary.com/drantho/image/upload/c_fill,w_670/v1616638401/mervin_graph_ijbjjr.png" />
                                     </div>
                                     <div className="panel">
-                                        <img src="https://res.cloudinary.com/drantho/image/upload/v1616638834/screenshot-penrv.com-2021.03.24-19_18_14_gpjplw.png" />
+                                        <img src="https://res.cloudinary.com/drantho/image/upload/c_fill,w_670/v1616638834/screenshot-penrv.com-2021.03.24-19_18_14_gpjplw.png" />
                                     </div>
                                 </Carousel>
                             </Col>
