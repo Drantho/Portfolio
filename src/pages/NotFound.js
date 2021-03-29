f5a920a9955fa0f2985e598becd7c4bef72b0ecb
Drantho <drantho@gmail.com>
2021-03-29 16:19:23 -0700

    deploy bug fix

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function NotFound() {
     return (
         <div>
-            <h1>Page Not Found</h1>
+            <h1>Page Not Found??</h1>
         </div>
     )
 }
